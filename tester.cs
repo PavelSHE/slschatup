f7063cc2cbbe1a8ef8dad9c84c39662b9f1dc884
Pavel Sh <pavel.Sheinfeld@gmail.com>
2018-10-16 10:36:42 +0300

    tester function

---
+++ tester.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+//custom 
+using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Collections.Generic;
+using Microsoft.Extensions.Primitives;
+
+namespace Arc.Function
+{
+    public static class tester
+    {
+        private static string configurationVariable(string name)
+            => System.Environment.GetEnvironmentVariable(name);
+
+        [FunctionName("tester")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            
+            string file = req.Query["file"];
+            string home = configurationVariable("HOME");
+            string publicFolder = configurationVariable("PUBLICFOLDER");
+
+            string name = req.Query["name"];
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+            name = name ?? "An0nym0us";
+
+            Dictionary<string,string> common = new Dictionary<string, string>();
+            common.Add("name",name);
+            common.Add("file",file);
+            common.Add("HOME",home);
+            common.Add("PUBLICFOLDER",publicFolder);
+
+            string json = JsonConvert.SerializeObject(common, Formatting.Indented);
+            
+            return (ActionResult)new OkObjectResult(json);
+        }
+    }
+}
