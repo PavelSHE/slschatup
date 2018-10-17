40849fc86633bb6b0c679bc535fe43d6d917529e
Pavel Sh <pavel.Sheinfeld@gmail.com>
2018-10-17 12:50:53 +0300

    header insted of claims

--- user.cs
+++ user.cs
@@ -11,6 +11,12 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Security.Claims;
 using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Security;
+using System.Web;
+using System.Collections.Specialized;
+using Microsoft.Extensions.Primitives;
 
 namespace Arc.Function
 {
@@ -25,25 +31,48 @@ namespace Arc.Function
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            try
-            {
-                JObject pJOtClaims = new JObject();
-            foreach(Claim curClaim in  ClaimsPrincipal.Current.Claims)
-            //ClaimsPrincipal.Current.Identities.First().Claims)
-            {
-                // if (Array.IndexOf(fields,curClaim.Type) >= 0)
-                // {   
-                //     string key = curClaim.Type.Substring(curClaim.Type.LastIndexOf("/") +1);
-                //     pJOtClaims.Add(key, new JValue(curClaim.Value));
-                // }
-                log.LogInformation(curClaim.Type + " : " + curClaim.Value);
+            IDictionary<string, StringValues> d = req.Headers;
+            foreach (KeyValuePair<string, StringValues> pair in d){
+                log.LogInformation("{0}:{1}",pair.Key,(string)pair.Value);
             }
-                return (ActionResult)new OkObjectResult(pJOtClaims);
-            }
-            catch (System.Exception)
-            {
-                return (ActionResult)new OkObjectResult("An0n? is it you?");            
-            }
+
+            return (ActionResult)new OkObjectResult("ok");
+            // Thread t = Thread.CurrentThread;
+            // HttpContext.
+            // if (!Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            // {
+            //     log.LogInformation("Not authenticated");
+            //     return (ActionResult)new OkObjectResult("An0n? is it you?");  
+            // }
+
+            // ClaimsIdentity identity = (Thread.CurrentPrincipal as ClaimsPrincipal)?.Identity as ClaimsIdentity;
+            // foreach (var claim in identity.Claims)
+            // {
+            // log.LogInformation($"{claim.Type} = {claim.Value}");
+            // }
+
+            // try
+            // {
+            //     JObject pJOtClaims = new JObject();
+            // ClaimsPrincipal me = ClaimsPrincipal.Current;
+            // IEnumerable<System.Security.Claims.ClaimsIdentity> mes = ClaimsPrincipal.Current.Identities;
+            // foreach(Claim curClaim in  ClaimsPrincipal.Current.Claims)
+            // //ClaimsPrincipal.Current.Identities.First().Claims)
+            // {
+            //     // if (Array.IndexOf(fields,curClaim.Type) >= 0)
+            //     // {   
+            //     //     string key = curClaim.Type.Substring(curClaim.Type.LastIndexOf("/") +1);
+            //     //     pJOtClaims.Add(key, new JValue(curClaim.Value));
+            //     // }
+            //     log.LogInformation(curClaim.Type + " : " + curClaim.Value);
+            // }
+            //     return (ActionResult)new OkObjectResult(pJOtClaims);
+            // }
+            // catch (System.Exception)
+            // {
+            //     log.LogError()
+            //     return (ActionResult)new OkObjectResult("An0n? is it you?");            
+            // }
             
         }
     }
